504d6041008d08700ace2b22ec81f5a834767154
Junsuk Oh <a811219@gmail.com>
2019-08-05 12:35:23 +0900

    메인 화면, 사진 선택 : image_picker 라이브러리

---
+++ lib/src/main_page.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_share_pic/src/map_page.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MainPage extends StatelessWidget {
+  Future<File> getImage() {
+    return ImagePicker.pickImage(source: ImageSource.gallery);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> _askedToLead() async {
+      switch (await showDialog<String>(
+          context: context,
+          builder: (BuildContext context) {
+            return SimpleDialog(
+              title: const Text('사진 업로드'),
+              children: <Widget>[
+                SimpleDialogOption(
+                  onPressed: () { Navigator.pop(context, 'pick'); },
+                  child: const Text('사진선택'),
+                ),
+                SimpleDialogOption(
+                  onPressed: () { Navigator.pop(context, 'take'); },
+                  child: const Text('사진찍기'),
+                ),
+              ],
+            );
+          }
+      )) {
+        case 'pick':
+          var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+          print(image);
+        // Let's go.
+        // ...
+
+          break;
+        case 'take':
+          var image = await ImagePicker.pickImage(source: ImageSource.camera);
+          print(image);
+        // ...
+          break;
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('사진 공유 앱'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("사진 업로드"),
+              color: Colors.orange,
+              textColor: Colors.white,
+              onPressed: () {
+                _askedToLead();
+              },
+            ),
+            RaisedButton(
+              child: Text("공유사진 보기"),
+              color: Colors.orange,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
