c7da0dffd6234d78be773240db6430f96e4c6cf6
Junsuk Oh <a811219@gmail.com>
2019-08-06 12:22:44 +0900

    DB 정보 가져와서 지도에 마커 추가

--- lib/src/map_page.dart
+++ lib/src/map_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -10,8 +11,10 @@ class MapPage extends StatefulWidget {
 
 class MapPageState extends State<MapPage> {
   Completer<GoogleMapController> _controller = Completer();
+
+  // 마커 코드
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
-  int _markerIdCounter = 1;
+  int _markerIdCounter = 0;
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -25,6 +28,27 @@ class MapPageState extends State<MapPage> {
       zoom: 19.151926040649414);
 
   @override
+  void initState() {
+    Firestore.instance.collection('share_pic').snapshots().listen((event) {
+      event.documents.forEach((document) {
+        _markerIdCounter++;
+        final Marker marker = Marker(
+          markerId: MarkerId('$_markerIdCounter'),
+          position: LatLng(document['lat'], document['lng']),
+          infoWindow: InfoWindow(title: document['description'], snippet: document['name']),
+        );
+
+        setState(() {
+          markers[marker.markerId] = marker;
+        });
+      });
+
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
@@ -36,7 +60,8 @@ class MapPageState extends State<MapPage> {
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-        markers: Set<Marker>.of(markers.values),
+        markers: Set<Marker>.of(markers.values),    // 마커 추가
+        myLocationEnabled: true,  // 현재 위치로 이동 버튼 추가
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _goToTheLake,
@@ -49,15 +74,5 @@ class MapPageState extends State<MapPage> {
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-
-    final Marker marker = Marker(
-      markerId: MarkerId('$_markerIdCounter'),
-      position: _kLake.target,
-      infoWindow: InfoWindow(title: 'test', snippet: '*'),
-    );
-
-    setState(() {
-      markers[marker.markerId] = marker;
-    });
   }
 }
